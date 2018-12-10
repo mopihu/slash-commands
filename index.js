f80f0ae9dbb0594ef928c6050d57f1b6815e443d
m0py <mopihu@gmail.com>
2018-12-11 00:54:16 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const Command = require('command')
-const config = require('./config.json')
+const config = require('./config.json');
 
-module.exports = function SlashCommands(dispatch) {
-  const command = Command(dispatch)
-
-  command.add(config.inspect, (name) => {
-    dispatch.toServer('C_REQUEST_USER_PAPERDOLL_INFO', 1, {
+module.exports = function SlashCommands(mod) {
+  mod.command.add(config.inspect, name => {
+    mod.send('C_REQUEST_USER_PAPERDOLL_INFO', 1, {
       name: name
-    })
-  })
+    });
+  });
 
-  command.add(config.invite, (name) => {
-    dispatch.toServer('C_REQUEST_CONTRACT', 1, {
+  mod.command.add(config.invite, name => {
+    mod.send('C_REQUEST_CONTRACT', 1, {
       type: 4,
       unk1: 0,
       unk2: 0,
@@ -19,44 +16,44 @@ module.exports = function SlashCommands(dispatch) {
       unk4: 0,
       name: name,
       data: 0
-    })
-  })
+    });
+  });
 
-  command.add(config.drop, () => {
-    dispatch.toServer('C_LEAVE_PARTY', 1, {})
-  })
+  mod.command.add(config.drop, () => {
+    mod.send('C_LEAVE_PARTY', 1, {});
+  });
 
-  command.add(config.disband, () => {
-    dispatch.toServer('C_DISMISS_PARTY', 1, {})
-  })
+  mod.command.add(config.disband, () => {
+    mod.send('C_DISMISS_PARTY', 1, {});
+  });
 
-  command.add(config.guildinvite, (name) => {
-    dispatch.toServer('C_INVITE_USER_TO_GUILD', 1, {
+  mod.command.add(config.guildinvite, name => {
+    mod.send('C_INVITE_USER_TO_GUILD', 1, {
       unk1: 0,
       unk2: 0,
       name: name
-    })
-  })
+    });
+  });
 
-  command.add(config.guildquit, () => {
-    dispatch.toServer('C_LEAVE_GUILD', 1, {})
-  })
+  mod.command.add(config.guildquit, () => {
+    mod.send('C_LEAVE_GUILD', 1, {});
+  });
 
-  command.add(config.guilddisband, () => {
-    dispatch.toServer('C_DESTROY_GUILD', 1, {})
-  })
+  mod.command.add(config.guilddisband, () => {
+    mod.send('C_DESTROY_GUILD', 1, {});
+  });
 
-  command.add(config.reset, () => {
-    dispatch.toServer('C_RESET_ALL_DUNGEON', 1, {})
-  })
+  mod.command.add(config.reset, () => {
+    mod.send('C_RESET_ALL_DUNGEON', 1, {});
+  });
 
   this.destructor = function() {
-    command.remove(config.inspect)
-    command.remove(config.invite)
-    command.remove(config.drop)
-    command.remove(config.guildinvite)
-    command.remove(config.guildquit)
-    command.remove(config.guilddisband)
-    command.remove(config.reset)
-  }
-}
+    mod.command.remove(config.inspect);
+    mod.command.remove(config.invite);
+    mod.command.remove(config.drop);
+    mod.command.remove(config.guildinvite);
+    mod.command.remove(config.guildquit);
+    mod.command.remove(config.guilddisband);
+    mod.command.remove(config.reset);
+  };
+};
